8bc0f2693e38e71f8fe2ccf069a4b2158b320653
Neftali Forero <neftaiof@gmail.com>
2024-02-02 13:28:38 -0500

    Modify key binding to confirm autocomplete in cmp

--- config/core/cmp.nix
+++ config/core/cmp.nix
@@ -30,12 +30,14 @@
       enable = true;
       snippet.expand = "luasnip";
       mapping = {
-        "<c-y>" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert })";
+        "<cr>" = "cmp.mapping.confirm({ select = false })";
+        "<tab>" = "cmp.mapping.select_next_item()";
+        #"<c-y>" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert })";
         "<c-d>" = "cmp.mapping.scroll_docs(-4)";
         "<c-f>" = "cmp.mapping.scroll_docs(4)";
         "<c-space>" = "cmp.mapping.complete()";
         "<c-e>" = "cmp.mapping.abort()";
-        "<c-n>" = "cmp.mapping.select_next_item()";
+        #"<c-n>" = "cmp.mapping.select_next_item()";
         "<c-p>" = "cmp.mapping.select_prev_item()";
       };
       sources = [
