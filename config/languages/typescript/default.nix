fd1b0329d2d32ab354a0a749a03acc6ab7b32948
Neftali Forero <neftaiof@gmail.com>
2024-02-14 17:42:28 -0500

    change indent to js ts files

--- config/languages/typescript/default.nix
+++ config/languages/typescript/default.nix
@@ -1,1 +1,22 @@
-{plugins.lsp.servers.tsserver.enable = true;}
+{
+  plugins.lsp.servers.tsserver.enable = true;
+
+  autoCmd = [
+    {
+      event = ["FileType"];
+      pattern = ["typescrip" "typescripreact" "javascript"];
+      callback = {
+        __raw = ''
+          function()
+            local set = vim.bo
+
+            set.tabstop = 2
+            set.softtabstop = 2
+            set.expandtab = true
+            set.shiftwidth = 2
+          end
+        '';
+      };
+    }
+  ];
+}
